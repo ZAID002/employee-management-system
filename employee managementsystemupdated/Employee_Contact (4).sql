607a39ef7ed0dfc880259ef8b2eefbe73842315b
ZAID UL HAQ <154435068+ZAID002@users.noreply.github.com>
2024-06-02 08:00:11 +0500

    Delete employee managementsystemupdated directory

--- employee managementsystemupdated/Employee_Contact (4).sql
+++
@@ -1,8 +0,0 @@
--- Table to store employee skills
-CREATE TABLE EmployeeSkills (
-    SkillID INT PRIMARY KEY,
-    EmployeeID INT,
-    SkillName VARCHAR(50),
-    FOREIGN KEY(EmployeeID)
-    REFERENCES Employees(EmployeeID)
-);
--- employee managementsystemupdated/Employee_Contact (7).sql
+++
@@ -1,8 +0,0 @@
--- Table to store department information
-CREATE TABLE Departments (
-    DepartmentID INT PRIMARY KEY,
-    DepartmentName VARCHAR(50),
-    EmployeeID INT,
-    FOREIGN KEY(EmployeeID)
-    REFERENCES Employees(EmployeeID)
-);
